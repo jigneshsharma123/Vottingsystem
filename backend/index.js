056ba5f7a1c5a821efe4abb3e1907a0ea311c8b3
jigneshsharma123 <92665503+jigneshsharma123@users.noreply.github.com>
2024-05-11 14:13:01 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+app.use(cors())
+mongoose.connect('mongodb://localhost:27017/voting');
+const Vote = mongoose.model('Vote', {option : String});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// handle vote subission
+app.post('/submit', async(req,res) => {
+    const {option} = req.body;
+    try {
+        await Vote.create({option});
+        res.send("vote subitted successfully");
+    } catch(err) {
+        console.log('Error in subitting vote:', err);
+        res.status(500).send('error submitting vote');
+    }
+});
+app.get('/results', async (req, res) => {
+    try {
+        const total = await Vote.countDocuments();
+        const results = await Vote.aggregate([
+            { $group: { _id: '$option', count: { $sum: 1 } } }
+        ]);
+        const formattedResults = results.reduce((acc, cur) => {
+            acc[cur._id] = cur.count;
+            return acc;
+        }, {});
+        res.json({ total, results: formattedResults });
+    } catch (error) {
+        console.error('Error fetching results:', error);
+        res.status(500).send('Error fetching results');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [option, setOption] = useState("");
+  const [totalResponses, setTotalResponses] = useState(0);
+  const [option1Responses, setOption1Responses] = useState(0);
+  const [option2Responses, setOption2Responses] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
+
+  const handleOptionChange = (e) => {
+    setOption(e.target.value);
+    setError(null);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!option) {
+      setError("Please select at least one option");
+      return;
+    }
+    try {
+      await axios.post(`${window.location.origin}/submit`, { option });
+      fetchResults();
+      setShowResults(true);
+    } catch (error) {
+      console.error("Error submitting vote:", error);
+    }
+  };
+
+  const fetchResults = async () => {
+    try {
+      const response = await axios.get(`${window.location.origin}/results`);
+      setTotalResponses(response.data.total);
+      setOption1Responses(response.data.results["Option 1"] || 0);
+      setOption2Responses(response.data.results["Option 2"] || 0);
+    } catch (error) {
+      console.error("Error fetching results:", error);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#f2f2f2",
+        padding: "20px",
+      }}
+    >
+      <h2 style={{ color: "#333", marginBottom: "10px" }}>
+        Are you a Vegetarian or Non-Vegetarian?
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: "20px" }}>
+          <input
+            type="radio"
+            id="option1"
+            name="option"
+            value="Option 1"
+            onChange={handleOptionChange}
+            style={{ marginRight: "10px" }}
+          />
+          <label htmlFor="option1" style={{ marginRight: "10px" }}>
+            Vegetarian
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="option2"
+            name="option"
+            value="Option 2"
+            onChange={handleOptionChange}
+            style={{ marginRight: "10px" }}
+          />
+          <label htmlFor="option2" style={{ marginRight: "10px" }}>
+            Non-Vegetarian
+          </label>
+        </div>
+        <button
+          type="submit"
+          style={{
+            backgroundColor: "#4CAF50",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            cursor: "pointer",
+            fontSize: "16px",
+          }}
+        >
+          Submit
+        </button>
+        {error && <p style={{ color: "red", marginBottom: "5px" }}>{error}</p>}
+      </form>
+      {showResults && (
+        <>
+          <h2 style={{ color: "#333", marginBottom: "10px" }}>
+            Voting Results
+          </h2>
+          <p style={{ marginBottom: "5px" }}>
+            Total Responses - {totalResponses}
+          </p>
+          <p style={{ marginBottom: "5px" }}>
+            Responses for Option 1 - {option1Responses}
+          </p>
+          <p style={{ marginBottom: "5px" }}>
+            Responses for Option 2 - {option2Responses}
+          </p>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
