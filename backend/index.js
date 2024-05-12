bc1643867a3116e4923628c96ebb124c244a3913
jigneshsharma123 <92665503+jigneshsharma123@users.noreply.github.com>
2024-05-12 12:31:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = 3001;
-app.use(cors(
-    {
-     origin : ["vottingsystem-frontend.vercel.app"],
-    methods : ["POST", "GET"],
-   credentials : true
-    }
-    
-))
+app.use(cors());
 mongoose.connect(
-  "mongodb+srv://jigneshsharma9868:JxZ8CxkSw7msVuhV@cluster0.6180gwf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ "mongodb://localhost:27017/voting"
 );
-const Vote = mongoose.model('Vote', {option : String});
-app.use(bodyParser.urlencoded({extended: true}));
+const Vote = mongoose.model("Vote", { option: String });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // handle vote subission
-app.post('/submit', async(req,res) => {
-    const {option} = req.body;
-    try {
-        await Vote.create({option});
-        res.send("vote subitted successfully");
-    } catch(err) {
-        console.log('Error in subitting vote:', err);
-        res.status(500).send('error submitting vote');
-    }
+app.post("/submit", async (req, res) => {
+  const { option } = req.body;
+  try {
+    await Vote.create({ option });
+    res.send("vote subitted successfully");
+  } catch (err) {
+    console.log("Error in subitting vote:", err);
+    res.status(500).send("error submitting vote");
+  }
 });
-app.get('/results', async (req, res) => {
-    try {
-        const total = await Vote.countDocuments();
-        const results = await Vote.aggregate([
-            { $group: { _id: '$option', count: { $sum: 1 } } }
-        ]);
-        const formattedResults = results.reduce((acc, cur) => {
-            acc[cur._id] = cur.count;
-            return acc;
-        }, {});
-        res.json({ total, results: formattedResults });
-    } catch (error) {
-        console.error('Error fetching results:', error);
-        res.status(500).send('Error fetching results');
-    }
+app.get("/results", async (req, res) => {
+  try {
+    const total = await Vote.countDocuments();
+    const results = await Vote.aggregate([
+      { $group: { _id: "$option", count: { $sum: 1 } } },
+    ]);
+    const formattedResults = results.reduce((acc, cur) => {
+      acc[cur._id] = cur.count;
+      return acc;
+    }, {});
+    res.json({ total, results: formattedResults });
+  } catch (error) {
+    console.error("Error fetching results:", error);
+    res.status(500).send("Error fetching results");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
